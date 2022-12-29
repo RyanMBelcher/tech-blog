4dd4e328b0d94128e93c9913268605d069c08da6
Ryan Belcher <ryanbelcher@MacBook-Pro.local>
2022-12-29 11:38:30 -0500

    finished setting up initial models file structure

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,1 +1,15 @@
 const router = require('express').Router();
+const { User, Blog, Comment } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                }
+            ]
+        })
+    }
+})
