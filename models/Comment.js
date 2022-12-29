5a5c5f845b30a6078b3b000e5fb9b3127e98b77a
Ryan Belcher <ryanbelcher@MacBook-Pro.local>
2022-12-29 11:48:40 -0500

    added blog model

--- models/Comment.js
+++ models/Comment.js
@@ -1,3 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
+
+class Comment extends Model { }
+
+Comment.init({})
+
+module.exports = Comment
