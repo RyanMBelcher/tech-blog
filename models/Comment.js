d4099f70f9655fec4c1c2bc8c440b848d81c0612
Ryan Belcher <ryanbelcher@MacBook-Pro.local>
2022-12-29 11:53:57 -0500

    added comment model

--- models/Comment.js
+++ models/Comment.js
@@ -3,6 +3,30 @@ const sequelize = require('../config/connection');
 
 class Comment extends Model { }
 
-Comment.init({})
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        comment: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        comment_user: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        blog_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'blog',
+                key: 'id'
+            },
+        },
+    },
+);
 
 module.exports = Comment
