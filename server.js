6134d3d60b85029bbaee69ba09f317c0abc1a1ff
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-01-10 15:53:48 -0500

    fixed some bugs

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: 'Super secret secret',
@@ -27,8 +30,6 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -41,3 +42,5 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening!'));
 });
+
+
