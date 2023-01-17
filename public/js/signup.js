bd2d022bab224038847eb1e3aae6afb7e2b77e12
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-01-17 09:25:41 -0500

    pushing all changes up to github

--- public/js/signup.js
+++ public/js/signup.js
@@ -16,16 +16,22 @@ const signUpFormHandler = async (event) => {
             document.location.replace('/');
         } else {
             const responseBody = await response.json()
+            console.log(responseBody);
             const hasEmailError = responseBody.errors.find(error =>
                 error.message === 'email must be unique'
             )
             const hasUsernameError = responseBody.errors.find(error =>
                 error.message === 'username must be unique'
             )
+            const hasPasswordError = responseBody.errors.find(error =>
+                error.message === 'Validation len on password failed'
+            )
             if (hasEmailError) {
                 alert('A user with this email address already exists. Please enter a new email address.')
             } else if (hasUsernameError) {
                 alert('A user with this username already exists. Please enter a new username.')
+            } else if (hasPasswordError) {
+                alert('Password must be at least 8 characters.')
             }
             else {
                 alert('Failed to sign up!')
